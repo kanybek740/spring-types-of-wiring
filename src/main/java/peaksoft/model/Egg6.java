182e502bf4db635a18d77af56f404c1800476ecb
kanybek_draft2 <kanybek1992@gmail.com>
2021-10-18 02:37:25 +0300

    Spring_homework_2

--- src/main/java/peaksoft/model/Egg6.java
+++ src/main/java/peaksoft/model/Egg6.java
@@ -1,12 +1,13 @@
 package peaksoft.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
 
     private final Needle7 needle7;
-
+    @Autowired
     public Egg6(Needle7 needle7){
         this.needle7 = needle7;
     }
--- src/main/java/peaksoft/model/Island2.java
+++ src/main/java/peaksoft/model/Island2.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class Island2 {
 
     private Wood3 wood;
-
+    @Autowired
     public Island2(Wood3 wood) {
         this.wood = wood;
     }
